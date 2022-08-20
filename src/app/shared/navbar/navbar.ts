7157426715283c8e862ee02003d820e956266c01
Ayush Saini <36878972+imayushsaini@users.noreply.github.com>
2022-08-21 00:01:11 +0530

    removed unused imports

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,16 +1,11 @@
 import {Component, EventEmitter, NgModule, OnDestroy, Output} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
-import {MatMenuModule} from '@angular/material/menu';
-import {RouterModule} from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {ThemePickerModule} from '../theme-picker';
 // import {VersionPickerModule} from '../version-picker';
 import {ThemeStorage} from '../theme-picker/theme-storage/theme-storage';
 import {StyleManager} from '../style-manager';
-import {HttpClientModule} from '@angular/common/http';
 import {Subscription} from 'rxjs';
 import {NavigationFocusService} from '../navigation-focus/navigation-focus.service';
 import {environment} from '../../../environments/environment';
